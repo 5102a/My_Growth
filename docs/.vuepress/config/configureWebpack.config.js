58c6b9456ba2d89d4a5eac6179c0f1b852cceaad
不是很开心c <49052946+1015355299@users.noreply.github.com>
2020-07-23 18:40:03 +0800

    增加评论

--- docs/.vuepress/config/configureWebpack.config.js
+++ docs/.vuepress/config/configureWebpack.config.js
@@ -1,12 +1,12 @@
 
 module.exports={
-  resolve: {
-    alias: {
-      // '~docs': path.normalize(__dirname+'/../../') ,
-      // '~vue-press': '../',
-      // '~my-press': '../../../',
-      // '~my-growth': '../../../../',
-      // '~public':'../public'
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     // '~docs': path.normalize(__dirname+'/../../') ,
+  //     // '~vue-press': '../',
+  //     // '~my-press': '../../../',
+  //     // '~my-growth': '../../../../',
+  //     // '~public':'../public'
+  //   },
+  // },
 }
